82a926a1fad2da51a6d052930d39c833060a5c58
jlbuild <juliabuildbot@gmail.com>
2022-03-01 11:04:50 +0000

    libigc_jll build 1.0.10395+0

--- src/wrappers/i686-linux-gnu-cxx03.jl
+++ src/wrappers/i686-linux-gnu-cxx03.jl
@@ -2,24 +2,14 @@
 export GenX_IR, iga, libiga, libigc, libigdfcl, libopencl_clang
 
 JLLWrappers.@generate_wrapper_header("libigc")
-JLLWrappers.@declare_executable_product(GenX_IR)
-JLLWrappers.@declare_executable_product(iga)
 JLLWrappers.@declare_library_product(libiga, "libiga32.so.1")
 JLLWrappers.@declare_library_product(libigc, "libigc.so.1")
 JLLWrappers.@declare_library_product(libigdfcl, "libigdfcl.so.1")
 JLLWrappers.@declare_library_product(libopencl_clang, "libopencl-clang.so.11")
+JLLWrappers.@declare_executable_product(GenX_IR)
+JLLWrappers.@declare_executable_product(iga)
 function __init__()
     JLLWrappers.@generate_init_header()
-    JLLWrappers.@init_executable_product(
-        GenX_IR,
-        "bin/GenX_IR",
-    )
-
-    JLLWrappers.@init_executable_product(
-        iga,
-        "bin/iga32",
-    )
-
     JLLWrappers.@init_library_product(
         libiga,
         "lib/libiga32.so",
@@ -42,6 +32,16 @@ function __init__()
         libopencl_clang,
         "lib/libopencl-clang.so.11",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        GenX_IR,
+        "bin/GenX_IR",
+    )
+
+    JLLWrappers.@init_executable_product(
+        iga,
+        "bin/iga32",
     )
 
     JLLWrappers.@generate_init_footer()
